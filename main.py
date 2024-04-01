c79a84aed927b4e70ce6aadff3ec74f01b031e05
Hussam Salameh <hussam.salameh@mail.huji.ac.il>
2024-04-01 13:39:43 +0300

    Adding all required paths

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+
+ORIGIN_RESUMES_PATH = "https://raw.githubusercontent.com/hussamsalamh/dataset/main/data2.csv"
+LABELED_RESUMES_PATH = "https://raw.githubusercontent.com/ghalebb/Resume_Analyzer/master/labeled_resumes.csv"
+RECOMMENDATION_RESUME_PATH = "https://raw.githubusercontent.com/ghalebb/Resume_Analyzer/master/recommendation.csv"
+
+
+if __name__ == '__main__':
+    pass
