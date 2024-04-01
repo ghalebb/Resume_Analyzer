a01af0d5ba80d0fc43753a3c7d4942eb4b90651d
Hussam Salameh <hussam.salameh@mail.huji.ac.il>
2024-04-01 13:24:53 +0300

    adding need libraries and make test file

--- Test/TestCode.py
+++ Test/TestCode.py
@@ -1,4 +1,3 @@
-from FirstPhase.preprocess import Preprocess
 from SecondPhase.Checker import Checker
 from SecondPhase.Response import Response
 
@@ -6,11 +5,6 @@ RESUME_RAW_TEXT_PATH = "RESUME_TEST.txt"
 LABEL_DATA_PATH = r"./Test/labelDataTmp.json"
 SECRET_KEY = "AIzaSyB1-RLEHFheGAlC80C4WfGReFBRN6sccPc"
 
-
-def checkPreprocess():
-    p = Preprocess(RESUME_RAW_TEXT_PATH)
-    cleanText, misspelling = p.preprocess()
-    return cleanText, misspelling
 
 def append_data(result):
     with open("output.txt", "w") as f:
@@ -25,7 +19,7 @@ def CheckSecondPhase():
     print("This the result after calling the checker class")
     print(results)
     print('---------------------------')
-    res = Response(results,"./SecondPhase/notes.json")
+    res = Response(results,"./SecondPhase/Prompts/notes.json")
     recommendations = res.generate_notes()
     print(recommendations)
     return recommendations
